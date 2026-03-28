29113e02f72e5631cc5875535cf472280050ade0
Artico <jean.benetti@peon.moda>
2026-03-28 00:16:14 -0300

    first commit

---
+++ src/main/java/com/articos/cancan/CancanApplication.java
@@ -0,0 +1,13 @@
+package com.articos.cancan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/articos/cancan/CancanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.articos.cancan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
